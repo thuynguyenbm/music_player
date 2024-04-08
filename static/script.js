f73e89605612d705134b05daaa74e964886df18a
Thi Minh Thuy Nguyen <thuy_ntm@flinters.vn>
2024-04-08 18:34:24 +0700

    add embeded video from YT

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 const playButton = document.querySelector('.playButton');
 const audioPlayer = document.querySelector('.audioPlayer');
-const audioData = audioPlayer.textContent
+const audioData = "blob:https://www.youtube.com/1b987a8e-946b-4234-aec3-cab2dae52624"
 
 // Create a Blob from the audio data
 const blob = new Blob([audioData], { type: 'audio/mp3' });
 
 // Create a Blob URL
-const blobUrl = URL.createObjectURL(blob);
+const blobUrl = "blob:https://www.youtube.com/1b987a8e-946b-4234-aec3-cab2dae52624"
 
 // Set the Blob URL as the audio source
 audioPlayer.src = blobUrl;
